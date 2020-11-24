8723e0b222255829246685b545abef778fff64be
alcarson1023 <alcarson1023@yahoo.com>
2020-11-24 13:42:49 -0600

    Added invert functionality

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+let allDivs = document.querySelector('.boxes')
+let reset = document.querySelector('#reset');
+let invert = document.querySelector('#invert');
+
 
 function generate(){
 
@@ -5,6 +9,7 @@ for(let i = 0; i < 256; i++){
     var div = document.createElement('div');
     div.setAttribute('class', 'boxes');
     div.setAttribute('id', `boxNumber${i}`)
+    div.style.backgroundColor = 'white'
     document.getElementById('container').appendChild(div);
 
     div.addEventListener('mouseenter', function(e){
@@ -12,9 +17,6 @@ for(let i = 0; i < 256; i++){
     })
 };
 }
-
-let allDivs = document.querySelector('.boxes')
-let reset = document.querySelector('#reset');
 
 reset.addEventListener('click', function() {
     
@@ -24,5 +26,22 @@ reset.addEventListener('click', function() {
     };
 
 });
+
+invert.addEventListener('click', function() {
+    
+    for(let i = 0; i < 256; i++){
+        let currentBox = document.querySelector(`#boxNumber${i}`)
+
+        if(currentBox.style.backgroundColor == 'white'){
+            currentBox.style.backgroundColor = 'black'
+        }
+
+        else{
+            currentBox.style.backgroundColor = 'white'
+        }
+    };
+    
+})
+
 
 generate()
